fb4b7edd09d9ba9ff4f28b26e7f49b1630c2b4ad
Kristina Tripak <tinatripak2002@gmail.com>
2026-07-26 17:44:14 -0400

    removing comments

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import { LuMail, LuMapPin, LuPhone, LuSend } from "react-icons/lu";
-// import { FORMSPREE_ENDPOINT } from "../config";
 import "./ContactUs.css";
 
 const initialForm = {
